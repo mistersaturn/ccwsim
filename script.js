b606e3785782b07cedacfe2cdcf8029b29850a83
dean <thinkpad>
2025-08-02 13:57:42 -0400

    add index.html, add script.js, img cleanup

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 window.showTab = function(tabId) {
   document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
   document.getElementById(tabId).classList.add('active');
@@ -1177,5 +1176,3 @@ function populateWrestlerDropdowns() {
     bSelect.add(optB);
   });
 }
-
-
